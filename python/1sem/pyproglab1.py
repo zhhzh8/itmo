dc159f5b5c11a4a62f1347411234209bfcd88e48
zhhzh <131249550+zhhzh8@users.noreply.github.com>
2025-09-06 14:39:26 +0300

    Update pyproglab1.py

--- python/1sem/pyproglab1.py
+++ python/1sem/pyproglab1.py
@@ -1,1 +1,20 @@
+try:
+    c = True
+    nums = [1, 2, 3, 4]
+    target = 7
+    for n in range(len(nums)):
+        n1 = nums[n]
+        for nn in range(len(nums)):
+            if nn == n:
+                pass
+            else:
+                n2 = nums[nn]
+                if n1 + n2 == target:
+                    print(n, nn)
+                    c = False
+    else:
+        if c:
+            print("Такой пары нет")
 
+except Exception as e:
+    print("Error:", e)
