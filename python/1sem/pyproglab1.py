d8014404ca45e86aa7c7c688c2b734b7ffb121c5
zhhzh <131249550+zhhzh8@users.noreply.github.com>
2025-09-19 16:48:19 +0300

    Добавление проверки функции

--- python/1sem/pyproglab1.py
+++ python/1sem/pyproglab1.py
@@ -1,20 +1,32 @@
-try:
-    c = True
-    nums = [1, 2, 3, 4]
-    target = 7
-    for n in range(len(nums)):
-        n1 = nums[n]
-        for nn in range(len(nums)):
-            if nn == n:
-                pass
-            else:
-                n2 = nums[nn]
-                if n1 + n2 == target:
-                    print(n, nn)
-                    c = False
-    else:
-        if c:
-            print("Такой пары нет")
+import unittest
 
-except Exception as e:
-    print("Error:", e)
+def sumoftwo(nums, target):
+    try:
+        c = True
+        for n in range(len(nums)):
+            n1 = nums[n]
+            for nn in range(len(nums)):
+                if nn == n:
+                    pass
+                else:
+                    n2 = nums[nn]
+                    if n1 + n2 == target:
+                        return [n, nn]
+        else:
+            if c:
+                return "Такой пары нет"
+
+    except Exception as e:
+        print("Error:", e)
+
+class TestMath(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(sumoftwo([3, 3], 6), [0, 1])
+
+    def test2(self):
+        self.assertEqual(sumoftwo([2,7,11,15], 9), [0, 1])
+
+    def test3(self):
+        self.assertEqual(sumoftwo([3,2,4], 6), [1, 2])
+
+unittest.main(argv=[''], verbosity=2, exit=False)
